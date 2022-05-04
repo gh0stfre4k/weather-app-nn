158035ef1e536eaa96e2d90ebe5dbdbf4ddc2838
gh0stfre4k <104137923+gh0stfre4k@users.noreply.github.com>
2022-05-04 02:45:50 +0200

    Update WeatherService.ts

--- src/api/WeatherService.ts
+++ src/api/WeatherService.ts
@@ -5,7 +5,7 @@ import {Weather, WeatherLocation} from '../api/Weather';
 //   throw new Error('No Open Weather API Key defined - ensure you set a variable called REACT_APP_OPEN_WEATHER_API_KEY')
 // }
 
-const keyQuery = `appid=a785af8f1b18b209267ac8daf685b224`
+const keyQuery = `appid=`
 const server = 'http://api.openweathermap.org/data/2.5';
 
 export async function searchLocation(term: string): Promise<WeatherLocation | undefined> {
